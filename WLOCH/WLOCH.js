0c291b0352f181ea888416f20576f76ef3011bcd
iki1uc <entertrainer@web.de>
2026-07-27 00:17:03 +0200

    Update WLOCH.js

--- WLOCH/WLOCH.js
+++ WLOCH/WLOCH.js
@@ -1,1 +1,10 @@
+import { WLOCH } from "../WLOCH/WLOCH.js";
+
+export function GATE_WLOCH(route) {
+  return WLOCH.stabilise({
+    gate: route,
+    axis: "ODD-SPRUNG",
+    stable: true
+  });
+}
 
