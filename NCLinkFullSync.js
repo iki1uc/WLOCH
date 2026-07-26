332006a311e3bc16a39cf0d1ebb08d2b1fa2e4b9
iki1uc <entertrainer@web.de>
2026-07-27 00:17:44 +0200

    Delete NCLinkFullSync.js

--- NCLinkFullSync.js
+++
@@ -1,9 +0,0 @@
-import { NCLinkFullSync } from "../NCLinkFullSync.js";
-
-export function GATE_LINK(gateValue) {
-  return NCLinkFullSync({
-    gate: gateValue,
-    route: "ODD-SPRUNG",
-    sync: true
-  });
-}
