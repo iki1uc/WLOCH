562d415bf4c03671b1527a82d818ad6e2a9a053d
iki1uc <entertrainer@web.de>
2026-07-27 00:17:35 +0200

    Update NCLinkFullSync.js

--- WLOCH/NCLinkFullSync.js
+++ WLOCH/NCLinkFullSync.js
@@ -1,1 +1,9 @@
+import { NCLinkFullSync } from "../NCLinkFullSync.js";
 
+export function GATE_LINK(gateValue) {
+  return NCLinkFullSync({
+    gate: gateValue,
+    route: "ODD-SPRUNG",
+    sync: true
+  });
+}
